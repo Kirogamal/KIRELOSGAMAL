48018d50c680c45d875021649de2f4808db52334
KIRELOSGAMAL <kirrogamal@gmail.com>
2024-12-05 14:03:09 +0200

    add project

---
+++ html css temp1/lab1.js
@@ -0,0 +1,80 @@
+
+1
+// let fname =prompt("what's your fname ?")
+// let lname =prompt("what's your lname ?")
+// let adress =prompt("what's your adress ?")
+
+// alert("Welcome" +" " + fname +" " +lname +" "+ adress)
+//////////
+
+
+
+2
+
+// var day=prompt("day numper") *1 ;
+// switch (day) {
+//   case 2:
+//    alert ("Sunday");
+//     break;
+//   case 3:
+//     alert ("Monday");
+//     break;
+//   case 4:
+//     alert("Tuesday");
+//     break;
+//   case 5:
+//     alert  ("Wednesday");
+//     break;
+//   case 6:
+//     alert  ("Thursday");
+//     break;
+//   case 7:
+//     alert  ("Friday");
+//     break;
+//   case  1:
+//     alert  ("Saturday");
+// }
+
+3
+
+
+// let message = prompt("Please enter a message:");
+
+//         // Loop through heading tags from <h1> to <h6>
+//         for (let i = 1; i <= 6; i++) {
+//             // Create a heading element
+//             let heading = document.createElement(`h${i}`);
+//             // Set the text of the heading element
+//             heading.textContent = message;
+//             // Append the heading element to the body
+//             document.body.appendChild(heading);
+//         }
+
+4
+
+
+
+5
+
+// var theage =prompt("give me your age ?")
+
+// let thedays= theage*365
+// console.log(thedays);
+
+// if(thedays%2==0){
+//     console.log("even");
+    
+    
+// }else{ 
+//     console.log("odd")
+// // }
+
+// var theage =prompt("give me your age ?")
+// if(theage!=""){
+//     alert(theage);
+// }else{
+//     alert("no result");
+// }
+
+
+7
